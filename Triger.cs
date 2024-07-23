daa074ad9f37a3d1725d62dd18106fa0cb087f4b
Sergeiphantom86 <96051914+Sergeiphantom86@users.noreply.github.com>
2024-07-23 13:25:07 +0300

    Update Triger.cs

--- Triger.cs
+++ Triger.cs
@@ -1,40 +1,21 @@
-using System.Collections;
 using UnityEngine;
+
+[RequireComponent(typeof(Alarm))]
 
 public class Triger : MonoBehaviour
 {
-    private float _target;
-    private float _minVolume = 0;
-    private float _stepUpVolume = 0.01f;
-    private Coroutine _coroutine;
-
-    private AudioSource _audioSource => GetComponent<AudioSource>();
+    private Alarm _alarm;
 
     private void Start()
     {
-        _audioSource?.Stop();
+        _alarm = GetComponent<Alarm>();
     }
 
     private void OnTriggerEnter(Collider criminal)
     {
         if (criminal.TryGetComponent(out Rogue _))
         {
-            _target = 1;
-
-            _audioSource.Play();
-
-            _coroutine = StartCoroutine(ChangeVolume());
-        }
-    }
-
-    private void OnTriggerStay(Collider criminal)
-    {
-        if (criminal.TryGetComponent(out Rogue _))
-        {
-            if (_audioSource.volume >= _target)
-            {
-                StopCoroutine(_coroutine);
-            }
+            _alarm.Turn();
         }
     }
 
@@ -42,23 +23,7 @@ public class Triger : MonoBehaviour
     {
         if (criminal.TryGetComponent(out Rogue _))
         {
-            _target = 0;
-        }
-    }
-
-    private IEnumerator ChangeVolume()
-    {
-        float delay = 0.1f;
-        
-        WaitForSeconds wait = new(delay);
-
-        while (enabled)
-        {
-            _audioSource.volume = _minVolume;
-
-            _minVolume = Mathf.MoveTowards(_minVolume, _target, _stepUpVolume);
-
-            yield return wait;
+            _alarm.ChangeTarget();
         }
     }
 }
